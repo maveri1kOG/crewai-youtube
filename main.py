934686e5fb75f9098ada903a283fdf8b884d78a1
maveri1kOG <147616701+maveri1kOG@users.noreply.github.com>
2024-03-24 18:40:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ researcher = Agent(
     role="Senior Research Analyst",
     goal='Uncover cutting-edge developments in AI and data science',
     backstory="""You work at a leading tech think tank.
-    Your expertise lies in identifying emerging trends.
-    You have a knack for dissecting complex data and presenting
+    Your expertise lipy es in identifying emerging trends.
+    You have a knack py for dissecting complex data and presenting
     actionable insights.""",
     verbose=True,
     allow_delegation=False,
